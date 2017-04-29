6c466c2e959dbc851bf3823d086fa12085889a26
Aparna Thirthapura <aparnathirthapura@Aparnas-MacBook-Air.local>
2017-04-29 17:23:46 -0400

    Second Commit

--- DosaApp/config/connection.js
+++ DosaApp/config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dosaDB"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else { 
+  mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dosaDB"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
